3664402b1c89cc7f5876798fc578c92ec238fa5b
Viet Nguyen <vietng1308@gmail.com>
2026-05-16 17:06:17 +0700

    Fix lỗi kết nối Database 127.0.0.1 trên Render

--- plant_shop/settings.py
+++ plant_shop/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -71,16 +72,17 @@ WSGI_APPLICATION = 'plant_shop.wsgi.application'
 
 # Database
 
+# Mặc định khi bạn code ở máy tính, nó sẽ dùng SQLite3
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'plant_shop_db',
-        'USER': 'postgres',
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Nhưng khi đẩy lên mạng (Render), nó sẽ tự động bắt lấy DATABASE_URL để kết nối PostgreSQL
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
 
 # Password validation
