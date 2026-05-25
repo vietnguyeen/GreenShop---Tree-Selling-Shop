f2a7ed2abdd265d08d0ec03f0c8d1429640dfa4b
Viet Nguyen <vietng1308@gmail.com>
2026-05-25 15:18:49 +0700

    fix

--- plant_shop/settings.py
+++ plant_shop/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
         'NAME':     os.getenv('POSTGRES_DB'      ),
         'USER':     os.getenv('POSTGRES_USER'     ),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST':     os.getenv('DB_HOST',           ),
-        'PORT':     os.getenv('DB_PORT',           ),
+        'HOST':     os.getenv('DB_HOST'           ),
+        'PORT':     os.getenv('DB_PORT'          ),
     }
 }
 
