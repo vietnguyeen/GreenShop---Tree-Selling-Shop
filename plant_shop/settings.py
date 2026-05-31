d056623861a57e1a658b2117f97945cc0fd5c4ae
Viet Nguyen <vietng1308@gmail.com>
2026-06-01 06:02:13 +0700

    update redis and celery

--- plant_shop/settings.py
+++ plant_shop/settings.py
@@ -135,7 +135,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') # Thay bằng email của bạn
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') # Thay bằng Mật khẩu ứng dụng (App Password)
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') 
 
 
