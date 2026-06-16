898e256b3d8411cec98d72f6f0891d5153207ef7
Viet Nguyen <vietng1308@gmail.com>
2026-06-17 02:43:17 +0700

    fix

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.apps import apps
 
-# Quét và lấy tất cả các models đang có trong app 'store'
 app_models = apps.get_app_config('store').get_models()
 
-# Tự động đăng ký hiển thị tất cả lên Admin
 for model in app_models:
     try:
         admin.site.register(model)
--- store/models.py
+++ store/models.py
@@ -37,9 +37,6 @@ class Plant(models.Model):
     price    = models.PositiveIntegerField(default=0,  verbose_name='Giá (VNĐ)')
     girth    = models.CharField(max_length=50,  null=True, blank=True, verbose_name='Đường kính gốc')
     height   = models.CharField(max_length=50,  null=True, blank=True, verbose_name='Chiều cao')
-
-    
-    # Đã thay thế hoàn toàn 'stock' bằng 'is_available'
     is_available = models.BooleanField(default=True, verbose_name='Còn hàng / Sẵn sàng giao')
 
     class Meta:
