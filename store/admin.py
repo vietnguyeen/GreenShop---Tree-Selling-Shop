84114f2631c42809cec9af65a53de6a345175b93
Viet Nguyen <vietng1308@gmail.com>
2026-06-16 14:08:35 +0700

    Remove deleted model PruningRequest from admin

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Plant, PruningRequest, Order, OrderItem, ServiceOrder
+from .models import Category, Plant, Order, OrderItem, ServiceOrder
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
